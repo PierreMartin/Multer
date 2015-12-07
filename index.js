9319ba67b0ea1652bf61272059a73c138d66ab10
PierreMrt <pierre.martin@socialshaker.com>
2015-12-07 14:21:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var express     = require('express');
+var multer      = require('multer');
+var	bodyParser  = require('body-parser');
+var	path        = require('path');
+
+var app = express();
+app.use(bodyParser.json());
+
+// view engine setup
+app.set('views', path.join(__dirname, '/'));
+app.set('view engine', 'jade');
+
+
+
+// Setup route for GET request, where the form will be displayed:
+app.get('/', function(req, res){
+    res.render('index');
+    //res.send('<p>some html</p>');
+});
+
+
+
+app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+	console.log(req.body); //form fields
+	/* example output:
+	{ title: 'abc' }
+	 */
+	console.log(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.status(204).end();
+});
+
+
+
+var port = 3001;
+app.listen(port, function() {
+        console.log('listening on port ' + port);
+    }
+);
